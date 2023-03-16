51eaf49e5c22f1b4b14f7aab72c1060e8c73820c
MasumHossain71 <masumhazi311@gmail.com>
2023-03-16 12:27:26 +0600

    stop watch builded

---
+++ app.js
@@ -0,0 +1,40 @@
+function{
+   var seconds = 00;
+   var tens = 00;
+   var appendSecond = document.getElementById('seconds');
+   var appendTens = document.getElementById('tens');
+   var startBtn = document.getElementById('start-btn');
+   var stopBtn = document.getElementById('stop-btn');
+   var resetBtn = document.getElementById('reset-btn');
+   var Interval;
+
+   startBtn.onclick = function(){
+      clearInterval(Interval);
+      Interval = setInterval(startTimer, 10);
+   }
+   stopBtn.onclick = function(){
+      clearInterval(Interval);
+   }
+   resetBtn.onclick = function(){
+      clearInterval(Interval);
+      seconds = '00';
+      tens = '00';
+      appendSecond.innerHTML = seconds;
+      appendTens.innerHTML = tens;
+   }
+
+   function startTimer(){
+      tens++;
+      if(tens<=9){
+         appendTens.innerHTML = '0'+tens;
+      }
+      if(tens>9){
+         appendTens.innerHTML = tens;
+      }
+      if(tens>99){
+         seconds++;
+         appendSecond.innerHTML = seconds;
+         
+      }
+   }
+}
